7c40681d66b487bcd352f893e9e54c8e55d4694a
Mouhssin El Madiouni <mouh_ssin1@yahoo.fr>
2015-10-07 11:35:47 -0300

    codeModifier

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -15,8 +15,8 @@ public class JavaApplication7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Mohamed");
-        System.out.println("");
+        System.out.println("Mohamed"); 
+        System.out.println("bonjour");
     }
     
 }
